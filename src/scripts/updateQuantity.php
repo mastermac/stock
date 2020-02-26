5da445ed9b2e5861a914ae9e197f34db85f061c9
Shubham Gupta <shubham_gupta5@dell.com>
2020-02-26 23:35:15 +0530

    Update updateQuantity.php

--- src/scripts/updateQuantity.php
+++ src/scripts/updateQuantity.php
@@ -2,21 +2,34 @@
 require 'db_config.php';
 $post = $_POST;
 
-if(empty($post['auth_token']) || $post['auth_token']!="cw\"o1s]zvN|6x@ShG4gKFeUX`slFoXNDA2~|2S6|BF=unZ\"SL'FNThq6O-@-f*)")
-    return "Authentication Error";
-if(empty($post['data']))
-    return "Invalid Request";
+if(empty($post['auth_token']) || $post['auth_token']!="cw\"o1s]zvN|6x@ShG4gKFeUX`slFoXNDA2~|2S6|BF=unZ\"SL'FNThq6O-@-f*)"){
+    echo "Authentication Error";
+    return;
+}
+if(empty($post['data'])){
+    echo "Invalid Request";
+    return;
+}
 
 $Query=Array();
 $dataRows=explode(";",$post['data']);
 $NumRows=count($dataRows);
 $stock=Array();
 $errorStockCodes=Array();
-for($i=1;$i<=$NumRows;$i++){
+$syncedItems=0;
+$blankRows=0;
+for($i=0;$i<$NumRows;$i++){
     $stock=Array();
-    $stock=explode(":",$dataRows[$i]);
-    if(count($stock)==2)
+    if(strlen(trim($dataRows[$i]))>0)
+        $stock=explode(":",$dataRows[$i]);
+    else{
+        $blankRows++;
+        continue;
+    }
+    if(count($stock)==2){
         $Query[]="UPDATE product SET curstock='".$stock[1]."' WHERE itemNo='".$stock[0]."'";
+        $syncedItems++;
+    }
     else
         array_push($errorStockCodes,$dataRows[$i]);
 }
@@ -25,8 +38,11 @@ $TheQueries[]=$Q;
 $mysqli=getConn();
 $mysqli->multi_query($Q);
 if(count($errorStockCodes)>0){
-    echo "Error in Items -- ".implode(';'. $errorStockCodes)."\n";
+    echo "Error in Items -- ".implode(';',$errorStockCodes)."\n";
 }
-echo ($NumRows - count($errorStockCodes))." out of ".$NumRows." Items Synced.";
+if($syncedItems>0)
+    echo $syncedItems." out of ".($NumRows-$blankRows)." Items Synced.";
+else
+    echo "Invalid Request";
 return;
 ?>
